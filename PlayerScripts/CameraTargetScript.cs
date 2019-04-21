8d872ca4955bc637099d38742d618dfc7636e4fb
Ethan Williams <ethan555williams@gmail.com>
2019-04-21 01:40:35 -0500

    Removed camera target, made cursor main controller

--- PlayerScripts/CameraTargetScript.cs
+++ PlayerScripts/CameraTargetScript.cs
@@ -4,43 +4,56 @@ using UnityEngine;
 
 public class CameraTargetScript : MonoBehaviour
 {
-    public Transform cursor;
-    public Transform target;
+    public GameObject cursor;
+
+    //public Transform target;
     public float speed = 0.5f;
     public float panSpeed = 10.0f;
 
+    void Awake() {
+        cursor = GameObject.Find("cursor");
+    }
+
     // Update is called once per frame
     void Update() {
+        Transform cursorTransform = cursor.transform;
+        Transform target = cursor.GetComponent<CursorMovement>().target;
         // If there is a target, move to between the target and the cursor
         if (target != null) {
             Vector3 tPos = target.position;
-            Vector3 curPos = cursor.position;
+            Vector3 curPos = cursorTransform.position;
             Vector3 cPos = transform.position;
             Vector3 destPos = target.position;
             Vector3 mPos = Input.mousePosition;
+
+            // Move to between the cursor and target
             destPos.x = (tPos.x + curPos.x)/2;
             destPos.y = (tPos.y + curPos.y)/2;
-            cPos.x = Mathf.Lerp(cPos.x, destPos.x, speed);
-            cPos.y = Mathf.Lerp(cPos.y, destPos.y, speed);
+            //cPos.x = Mathf.Lerp(cPos.x, destPos.x, speed);
+            //cPos.y = Mathf.Lerp(cPos.y, destPos.y, speed);
+            cPos.x = destPos.x;
+            cPos.y = destPos.y;
+
             transform.position = cPos;
         } else {
             // Otherwise, just move by control and by cursor
-            Vector3 curPos = cursor.position;
+            Vector3 curPos = cursorTransform.position;
             Vector3 cPos = transform.position;
             Vector3 v0Pos = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
             Vector3 v1Pos = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
-            // cPos.x = Mathf.Lerp(cPos.x, curPos.x, speed);
-            // cPos.y = Mathf.Lerp(cPos.y, curPos.y, speed);
-            // transform.position = cPos;
+
+            // Move by keyboard input
             cPos.x += Input.GetAxis("Horizontal") * panSpeed * Time.deltaTime;
             cPos.y += Input.GetAxis("Vertical") * panSpeed * Time.deltaTime;
 
+            // Move when the cursor is at the edge of the viewport
             if (curPos.x <= v0Pos.x || curPos.x >= v1Pos.x) {// < screenSize.x/4 || mPos.x > 3*screenSize.x/4) {
                 cPos.x += Mathf.Sign(curPos.x - v0Pos.x - 1) * panSpeed * Time.deltaTime;//Mathf.Clamp(mPos.x - screenSize.x/2,// - mouse_pos.x,-mouseSpeed, mouseSpeed);
             }
             if (curPos.y <= v0Pos.y || curPos.y >= v1Pos.y) {//  < screenSize.y/4 || mPos.y > 3*screenSize.y/4) {
                 cPos.y += Mathf.Sign(curPos.y - v0Pos.y - 1) * panSpeed * Time.deltaTime;//Mathf.Clamp(mPos.y - screenSize.y/2,// - mouse_pos.y, -mouseSpeed, mouseSpeed);
             }
+
             transform.position = cPos;
         }
     }
--- PlayerScripts/CursorMovement.cs
+++ PlayerScripts/CursorMovement.cs
@@ -7,7 +7,7 @@ public class CursorMovement : MonoBehaviour
 
     public float maxSpeed = 10.0f;
     public float mouseSpeed = 10.0f;
-    public float rotateSpeed = 1.0f;
+    public float rotateSpeed = 20.0f;
     public Transform target = null;
     public float aimDist = 1.0f;
 
@@ -21,25 +21,23 @@ public class CursorMovement : MonoBehaviour
     // Update is called once per frame
     void Update() {
         Vector3 pos = transform.position;
-        Vector3 screenSize = Camera.main.ScreenToViewportPoint(new Vector3(Screen.width, Screen.height, 0));
-        Vector3 mPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        Vector3 trueMPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         // If there is no target, then freecam
         if (target == null) {
             // Move the cursor
             // Returns a float from -1.0 to 1.0
-            pos.x += Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
-            pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
             
-            pos.x = trueMPos.x;//mPos.x;
-            pos.y = trueMPos.y;//mPos.y;
+            pos.x = mPos.x;//mPos.x;
+            pos.y = mPos.y;//mPos.y;
 
             transform.position = pos;
-        } else {
+        } else if (Input.GetAxis("Fire2") == 1) {
             // If there is a target, then cannot move too far away
             // Move the cursor
             Vector3 tPos = target.position;
-            mPos = mPos - screenSize/2;
+            Cursor.visible = false;
+
+            //mPos = mPos - screenSize/2;
             // Returns a float from -1.0 to 1.0
             /*pos.x += maxSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
             pos.y += maxSpeed * Time.deltaTime * Input.GetAxis("Vertical");
@@ -50,16 +48,20 @@ public class CursorMovement : MonoBehaviour
                 -mouseSpeed, mouseSpeed);
             mouse_pos.x = mPos.x;
             mouse_pos.y = mPos.y;*/
+            pos.x += Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;//mPos.x;
+            pos.y += Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;//mPos.y;
 
             Vector3 angle = new Vector3(pos.x - tPos.x, pos.y - tPos.y, 0);//direction;
 
             //angle = Quaternion.Euler(0, 0, Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime) * angle;
-            angle = mPos - tPos;
-            //if (angle.magnitude > aimDist) {
-            angle = angle.normalized * aimDist;
+            //angle = mPos - tPos;
+           // if (angle.magnitude > aimDist) {
+                angle = angle.normalized * aimDist;
             //}
 
             transform.position = tPos + angle;
+        } else {
+            Cursor.visible = true;
         }
     }
 }
